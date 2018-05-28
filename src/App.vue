e7835b020125d0391be690b5dd57bb92a27dff26
bkbest90 <wthnfbest@gmail.com>
2018-05-29 03:43:14 +0700

    Very nice shop are finish

--- src/App.vue
+++ src/App.vue
@@ -26,13 +26,57 @@
                     {{product.productName}}<br/>
   									<p class="price"><h5>{{product.price}} ฿</h5> </p>
                     {{product.amount}} left <br>
-                    <button type="button" class="btn btn-outline-primary" v-if="product.amount !==0" @click="addToCart(product.productName,product.price,product['.key'],product.amount,product.imgLink)">Add to cart</button>
-							</li>
+                    <button type="button" class="btn btn-outline-primary" v-if="adminF == false && product.amount !==0 && loginState== true"
+                    @click="addToCart(product.productName,product.price,product['.key'],product.amount,product.imgLink)">Add to cart</button>
+                    <button type="button" class="btn btn-outline-warning" v-if="adminF && loginState== true"
+                    @click="editProduct(product.productName,product.price,product.amount,product.imgLink,product['.key'])">Edit</button>
+                    <button type="button" class="btn btn-outline-danger" v-if="adminF && loginState== true"
+                    @click="deleteProduct(product['.key'])">Delete</button>
+              </li>
 						</ul>
 						<hr>
 					</div>
-          <div class="col-3" style="text-align: center" >
-            <h2>Your Cart</h2>
+          <!-- login box -->
+          <div class="col-3" style="text-align: center" v-if="loginState == false && registerState == false" >
+            <h2>Login</h2>
+            <div class="col">
+              <input type="text" class="form-control" placeholder="Email" v-model="email" >
+              <br>
+            </div>
+            <div class="col">
+              <input type="password" class="form-control" placeholder="Password" v-model="passEmail" >
+            </div>
+            <br>
+            <button type="button" class="btn btn-outline-success" @click="checkUserLogin()">Login</button>
+            <button type="button" class="btn btn-outline-warning" @click="changeStateRe()">Register</button>
+                <!-- <button @click="removeTodoFire(todo['.key'])">X</button> -->
+          </div>
+          <!-- login box -->
+          <!-- register box -->
+          <div class="col-3" style="text-align: center" v-if="registerState == true" >
+            <h2>Register</h2>
+            <div class="col">
+              <input type="text" class="form-control" placeholder="Username" v-model="userNameRe" >
+            </div>
+            <br>
+            <div class="col">
+              <input type="text" class="form-control" placeholder="Email" v-model="emailRe" >
+              <br>
+            </div>
+            <div class="col">
+              <input type="password" class="form-control" placeholder="Password" v-model="passEmailRe" >
+            </div>
+            <br>
+
+            <button type="button" class="btn btn-outline-primary" @click="registerUser">Register</button>
+            <button type="button" class="btn btn-outline-danger" @click="changeStateRe">Cancel</button>
+                <!-- <button @click="removeTodoFire(todo['.key'])">X</button> -->
+          </div>
+          <!-- register box -->
+          <!-- Your cart -->
+          <div class="col-3" style="text-align: center" v-if="loginState== true && adminF == false" >
+            <h2>{{userName}}</h2>
+            Your Cart <br><br>
              <div v-for="(item , index) in items">
                <ul>
                  <li class="list-group-item" style="width:350px; margin-bottom: 10px; " ><img :src="item.imgLink" width="50" height="50"> {{item.productName}} : {{item.price}} ฿ <br> {{item.amount}}<br>
@@ -42,36 +86,61 @@
 
             All of these : <strong>{{cost}} ฿ </strong><br>
             <button type="button" class="btn btn-outline-success" @click="checkOut">Check out</button>
+            <button type="button" class="btn btn-outline-danger" @click="logout">Logout</button>
+          </div>
+          <!-- Your cart -->
+          <!-- Admin -->
+          <div class="col-3" style="text-align: center" v-if="adminF" >
+            <h2>{{userName}}</h2>
+
+            <button type="button" class="btn btn-outline-success" @click="addProductStateChange">Add product</button>
+            <button type="button" class="btn btn-outline-danger" @click="logout">Logout</button>
+            <div class="" v-if="addProductState" >
+              <div>
+                <br> <strong>Add product</strong> <br>
+              </div>
+
+                <div class="col">
+                  <input type="text" class="form-control" placeholder="Name" v-model="n" >
+                </div><br>
+                <div class="col">
+                  <input type="number" class="form-control" min="1" placeholder="Price" v-model="p" >
+                </div><br>
+                <div class="col">
+                  <input type="number" class="form-control" min="1" placeholder="Amount" v-model="a" >
+                </div><br>
+                <div class="col">
+                  <input type="text" class="form-control" placeholder="Image link" v-model="l" >
+                </div><br>
+                <button class="btn btn-success" @click="addProduct"> Add </button>
+                <button class="btn btn-danger" @click="addProductState = false"> Cancel </button>
+
+            </div>
+            <div class="" v-if="editState" >
+              <div>
+                <br> <strong>Edit product</strong> <br>
+              </div>
+
+                <div class="col">
+                  <input type="text" class="form-control" placeholder="Name" v-model="nEdit" >
+                </div><br>
+                <div class="col">
+                  <input type="number" class="form-control" min="1" placeholder="Price" v-model="pEdit" >
+                </div><br>
+                <div class="col">
+                  <input type="number" class="form-control" min="1" placeholder="Amount" v-model="aEdit" >
+                </div><br>
+                <div class="col">
+                  <input type="text" class="form-control" placeholder="Image link" v-model="lEdit" >
+                </div><br>
+                <button class="btn btn-warning" @click="submitEditProduct"> Edit </button>
+                <button class="btn btn-danger" @click="editState = false"> Cancel </button>
+
+            </div>
                 <!-- <button @click="removeTodoFire(todo['.key'])">X</button> -->
           </div>
+          <!-- Admin -->
 				 </div>
-        <div class="" v-if="inputPassword!=passwordForAdd" style="margin-bottom: 30px">
-          <h2>Enter password to add product</h2>
-          <input type="password" class="form-control" placeholder="Password" v-model="inputPassword" >
-        </div>
-      <div class="" v-if="inputPassword==passwordForAdd" >
-        <div>
-          <h1>Add product</h1> <br>
-        </div>
-
-        <div class="row" style="margin-bottom: 30px">
-
-          <div class="col">
-            <input type="text" class="form-control" placeholder="Name" v-model="n" >
-          </div>
-          <div class="col">
-            <input type="number" class="form-control" min="1" placeholder="Price" v-model="p" >
-          </div>
-          <div class="col">
-            <input type="number" class="form-control" min="1" placeholder="Amount" v-model="a" >
-          </div>
-          <div class="col">
-            <input type="text" class="form-control" placeholder="Image link" v-model="l" >
-          </div>
-          <button class="btn btn-success" @click="addProduct"> Add </button>
-          <button class="btn btn-danger" @click="changePassword"> Cancel </button>
-        </div>
-      </div>
 		</div>
 		</div>
     <!-- firebase -->
@@ -84,6 +153,8 @@
 </template>
 <script>
 import firebase from 'firebase'
+import swal from 'sweetalert2'
+
 var config = {
   apiKey: 'AIzaSyBPDmTmUaMfH0l666pnCk3BElbpdVh47o0',
   authDomain: 'onlineshop-best.firebaseapp.com',
@@ -95,12 +166,14 @@ var config = {
 var firebaseApp = firebase.initializeApp(config)
 var db = firebaseApp.database()
 var todosRef = db.ref('todos')
+var UserRef = db.ref('users')
 var productRef = db.ref('products')
 export default {
   name: 'App',
   firebase: {
     todosFire: todosRef,
-    productFire: productRef
+    productFire: productRef,
+    userFire: UserRef
   },
   data () {
     return {
@@ -109,19 +182,183 @@ export default {
       price: '',
       addProductButtonState: false,
       inputPassword: '',
-      passwordForAdd: '123456',
       authorized: false,
       onAdd: false,
       p: null,
       n: null,
       l: null,
       a: null,
+      pEdit: null,
+      nEdit: null,
+      lEdit: null,
+      aEdit: null,
+      keyEdit: null,
+      emailRe: '',
+      passEmailRe: '',
+      userNameRe: '',
+      email: '',
+      passEmail: '',
+      userName: '',
+      adminF: false,
       cost: 0,
       startA: 1,
+      loginState: false,
+      registerState: false,
+      addProductState: false,
+      editState: false,
       items: []
     }
   },
   methods: {
+    addProductStateChange () {
+      this.addProductState = true
+      this.editState = false
+    },
+    submitEditProduct () {
+      swal({
+        title: 'Are you sure?',
+        text: "After this you can edit anytime",
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#ff9900',
+        cancelButtonColor: '#adad85',
+        confirmButtonText: 'Yes, edit it!'
+      }).then((result) => {
+        if (result.value) {
+          db.ref('products/' + this.keyEdit).set({
+            productName: this.nEdit,
+            price: this.pEdit,
+            imgLink: this.lEdit,
+            amount: this.aEdit
+          })
+          this.nEdit = null
+          this.pEdit = null
+          this.aEdit = null
+          this.lEdit = null
+          this.keyEdit = null
+          this.editState = false
+          swal(
+            'Edited!',
+            'Your product has been edited.',
+            'success'
+          )
+        }
+      })
+    },
+    editProduct (name,price,amount,link,key) {
+      this.editState = true
+      this.addProductState = false
+      this.nEdit = name
+      this.pEdit = price
+      this.aEdit = amount
+      this.lEdit = link
+      this.keyEdit = key
+      window.scrollTo(450, 450)
+    },
+    deleteProduct (key) {
+      swal({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#d33',
+        cancelButtonColor: '#adad85',
+        confirmButtonText: 'Yes, delete it!'
+      }).then((result) => {
+        if (result.value) {
+          db.ref('products/' + key).remove()
+          swal(
+            'Deleted!',
+            'Your product has been deleted.',
+            'success'
+          )
+        }
+      })
+    },
+    logout () {
+      this.loginState = false
+      this.email = ''
+      this.userName = ''
+      this.passEmail = ''
+      this.adminFunction = false
+    },
+    checkUserLogin () {
+      for (var i = 0; i < this.userFire.length; i++) {
+        if (this.email == this.userFire[i].email && this.passEmail == this.userFire[i].password) {
+          this.loginState = true
+          this.userName = this.userFire[i].username
+          this.adminF = this.userFire[i].adminFunction
+          if (this.userFire[i].adminFunction) {
+            swal({
+              position: 'center',
+              type: 'success',
+              title: 'Welcome back Admin',
+              showConfirmButton: false,
+              timer: 1500
+            })
+          } else {
+            swal({
+              position: 'center',
+              type: 'success',
+              title: 'Welcome :)',
+              showConfirmButton: false,
+              timer: 1500
+            })
+          }
+        }
+      }
+      if(this.loginState == false) {
+        swal(
+          'Warning!',
+          'Invalid email or password.',
+          'warning'
+          )
+      }
+    },
+    registerUser () {
+      var pattMail = /.+@\w+\..+/g
+      var pattPass = /.{6}/g
+      var mailMatch = pattMail.test(this.emailRe)
+      var passMatch = pattPass.test(this.passEmailRe)
+      if (mailMatch) {
+        if (passMatch) {
+          UserRef.push({
+            email: this.emailRe,
+            password: this.passEmailRe,
+            username: this.userNameRe,
+            adminFunction: false
+          })
+          swal(
+            'Thank you!',
+            'Registeration successed',
+            'success'
+            )
+          this.emailRe = ''
+          this.passEmailRe = ''
+          this.registerState = false
+          } else {
+            swal(
+              'Warning!',
+              'Please enter at least 6 characters for password.',
+              'warning'
+              )
+            }
+
+        } else {
+          swal(
+            'Warning!',
+            'Please enter the correct email!',
+            'warning'
+            )
+        }
+    },
+    changeStateRe () {
+      if (this.registerState == true) {
+        this.registerState = false
+      } else {
+        this.registerState = true
+      }
+    },
     changePassword () {
       this.inputPassword = ''
     },
@@ -137,13 +374,25 @@ export default {
       this.items.splice(index, 1)
     },
     checkOut () {
-      this.items = []
-      this.cost = 0
-      swal(
-        'Thank you!',
-        'come back again :)',
-        'success'
-        )
+      swal({
+        title: 'Are you sure?',
+        text: "You won't be able to refund!",
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#33cc33',
+        cancelButtonColor: '#adad85',
+        confirmButtonText: 'Yes, I buy all of these!'
+      }).then((result) => {
+        if (result.value) {
+          this.items = []
+          this.cost = 0
+          swal(
+            'Thank you!',
+            'come back again :)',
+            'success'
+            )
+        }
+      })
     },
     addToCart (name,price,key,amount,link) {
       console.log(amount)
@@ -203,10 +452,6 @@ export default {
       }
     },
     addProduct () {
-      console.log(this.p)
-      console.log(this.n)
-      console.log(this.a)
-      console.log(this.l)
       if (this.n!==null||this.p!==null||this.a!==null||this.l!==null) {
         if (this.n!==null) {
           if (this.p!==null&&this.p>=0) {
